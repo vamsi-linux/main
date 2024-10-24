1fbc294e2f4abe9904df79b4f1bd88a81562cc32
vamsi-linux <royalvamsi36@gmail.com>
2024-10-24 14:18:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "this" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "m5.large"
+  host_resource_group_arn = "arn:aws:resource-groups:us-west-2:012345678901:group/win-testhost"
+  tenancy                 = "host"
+}
